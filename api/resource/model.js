ab77ea662f7f45b66afc3df84d75e99d8504733b
bugsbrog <bugsbrog@gmail.com>
2022-02-18 12:10:03 -0700

    Done with resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,11 +1,12 @@
 const db = require('../../data/dbConfig')
 
 async function getAllResources() {
-
+    await db('resources')
 }
 
 async function create(resource) {
-
+    const [resource_id] = await db('resources').insert(resource)
+    return db('resources').where('resource_id', resource_id).first()
 }
 
 module.exports = {
