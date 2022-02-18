b6da4b6d7a9bdbfdbccf84ff2fcaa624b77eb136
bugsbrog <bugsbrog@gmail.com>
2022-02-17 19:32:01 -0700

    Done with migration file

--- data/migrations/20220218021116_first-migration.js
+++ data/migrations/20220218021116_first-migration.js
@@ -1,6 +1,6 @@
 exports.up = async function(knex) {
     await knex.schema
-        .createTable('project', (table) => {
+        .createTable('projects', (table) => {
             table.increments('project_id')
             table.string('project_name', 128)
                 .notNullable()
@@ -8,14 +8,14 @@ exports.up = async function(knex) {
             table.boolean('project_completed')
                 .defaultTo(false)
         })
-        .createTable('resource', (table) => {
+        .createTable('resources', (table) => {
             table.increments('resource_id')
             table.string('resource_name', 128)
                 .notNullable()
                 .unique()
             table.string('resource_description', 128)
         })
-        .createTable('task', (table) => {
+        .createTable('tasks', (table) => {
             table.increments('task_id')
             table.string('task_description', 128)
                 .notNullable()
@@ -30,8 +30,30 @@ exports.up = async function(knex) {
                 .onDelete('RESTRICT')
                 .onUpdate('RESTRICT')
         })
+        .createTable('project_resources', (table) => {
+            table.increments('project_resource_id')
+            table.string('project_resource_assignment')
+            table.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resource_id')
+                .inTable('resources')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+        })
 };
 
 exports.down = async function(knex) {
-  
+  await knex.schema
+      .dropTableIfExists('project_resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
 };
