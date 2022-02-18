6d81dfa82b5da78fbafdf9520fc9b8ba6b655986
bugsbrog <bugsbrog@gmail.com>
2022-02-18 12:06:47 -0700

    Finally got all project routes done. Forgot to add server.use(express.json()) and that's why my post wasn't working

--- api/project/model.js
+++ api/project/model.js
@@ -1,11 +1,31 @@
 const db = require('../../data/dbConfig')
 
 async function getAllProjects() {
+    const projs = await db('projects')
 
+    const projMap = projs.map(proj => {
+        if (proj.project_completed === 0) {
+            return {
+                project_id: proj.project_id,
+                project_name: proj.project_name,
+                project_description: proj.project_description,
+                project_completed: false
+            }
+        } else {
+            return {
+                project_id: proj.project_id,
+                project_name: proj.project_name,
+                project_description: proj.project_description,
+                project_completed: true
+            }
+        }
+    })
+    return projMap
 }
 
 async function create(project) {
-
+    const [project_id] = await db('projects').insert(project)
+    return db('projects').where('project_id', project_id).first()
 }
 
 module.exports = {
--- api/project/router.js
+++ api/project/router.js
@@ -2,11 +2,35 @@ const router = require('express').Router()
 const Project = require('./model')
 
 router.get('/', async (req, res, next) => {
-
+    try {
+        const proj = await Project.getAllProjects()
+        res.json(proj)
+    } catch (err){
+        next(err)
+    }
 })
 
 router.post('/', async (req, res, next) => {
-
+        try {
+            const createProj = await Project.create(req.body)
+            if (createProj.project_completed === 0) {
+                res.status(201).json({
+                    project_id: createProj.project_id,
+                    project_name: createProj.project_name,
+                    project_description: createProj.project_description,
+                    project_completed: false
+                })
+            } else {
+                res.status(201).json({
+                    project_id: createProj.project_id,
+                    project_name: createProj.project_name,
+                    project_description: createProj.project_description,
+                    project_completed: true
+                })
+            }
+        } catch (err) {
+            next(err)
+        }
 })
 
 module.exports = router
